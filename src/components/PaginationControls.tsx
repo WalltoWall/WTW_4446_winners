8beacdd0181d1af577c0e387fc1eb51425bff98c
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-04-08 16:29:03 -1000

    feat: better pagination information

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -20,11 +20,12 @@ const variants = {
 
 type DirectionButtonProps = ViewProps & {
   variant: keyof typeof variants
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  disabled?: boolean
 }
 
 const DirectionButton = ({
   variant: variantName,
+  disabled = false,
   ...props
 }: DirectionButtonProps) => {
   const variant = variants[variantName]
@@ -32,9 +33,10 @@ const DirectionButton = ({
   return (
     <View
       as="button"
+      disabled={disabled}
       {...props}
       css={mq({
-        backgroundColor: t.c.Gray70,
+        backgroundColor: disabled ? t.c.Gray90 : t.c.Gray70,
         width: linearScale('1.125rem', '1.5rem'),
         height: linearScale('1.125rem', '1.5rem'),
         borderRadius: '50%',
@@ -42,6 +44,7 @@ const DirectionButton = ({
         alignItems: 'center',
         justifyContent: 'center',
         transitionProperty: 'background-color',
+        pointerEvents: disabled ? 'none' : 'auto',
         '&:hover, &:focus': {
           backgroundColor: t.c.Gray60,
         },
@@ -50,7 +53,7 @@ const DirectionButton = ({
       <Icon
         name={variant.iconName}
         css={mq({
-          color: t.c.White,
+          color: disabled ? t.c.Gray95 : t.c.White,
           width: ['0.3rem', '0.4rem', '0.5rem'],
           transform: `translateX(${variant.svgOffsetX})`,
         })}
@@ -76,11 +79,13 @@ export const PaginationControls = ({
     .fill(undefined)
     .map((_, i) => i + 1)
 
+  const hasPreviousPage = currentPage > 1
   const decPage = useCallback(() => setPage(Math.max(currentPage - 1, 1)), [
     currentPage,
     setPage,
   ])
 
+  const hasNextPage = currentPage < totalPages
   const incPage = useCallback(
     () => setPage(Math.min(currentPage + 1, totalPages)),
     [currentPage, setPage, totalPages],
@@ -96,7 +101,11 @@ export const PaginationControls = ({
         alignItems: 'center',
       })}
     >
-      <DirectionButton variant="previous" onClick={decPage} />
+      <DirectionButton
+        variant="previous"
+        disabled={!hasPreviousPage}
+        onClick={decPage}
+      />
       <ul
         css={mq({
           display: 'grid',
@@ -121,7 +130,11 @@ export const PaginationControls = ({
           </li>
         ))}
       </ul>
-      <DirectionButton variant="next" onClick={incPage} />
+      <DirectionButton
+        variant="next"
+        disabled={!hasNextPage}
+        onClick={incPage}
+      />
     </View>
   )
 }
