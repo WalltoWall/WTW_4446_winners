61d36447796746ea904c8e9fcdc447d547d8018f
Anthony Yarbrough <asyarb@hawaii.edu>
2020-04-08 18:08:45 -1000

    feat: title case

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -7,7 +7,7 @@ export type CardListProps = ViewProps & {
   columns?: number[]
 }
 
-const margin = [0.8125, 1.5]
+const MARGIN = [0.8125, 1.5]
 export const CardList = ({
   children,
   columns = [1],
@@ -22,7 +22,7 @@ export const CardList = ({
       css={mq({
         display: 'flex',
         flexWrap: 'wrap',
-        margin: margin.map(m => `-${m}rem`),
+        margin: MARGIN.map(m => `-${m}rem`),
         alignItems: 'stretch',
         // If we have enough items to create more than one row, left align.
         justifyContent: columns.map(qty =>
@@ -35,11 +35,11 @@ export const CardList = ({
           key={i}
           css={mq({
             display: 'flex',
-            margin: margin.map(m => `${m}rem`),
+            margin: MARGIN.map(m => `${m}rem`),
             width: columns.map(
               (qty, i) =>
                 `calc(${(100 / qty).toPrecision(4)}% - ${
-                  (i === 0 ? margin[0] : margin[1]) * 2
+                  (i === 0 ? MARGIN[0] : MARGIN[1]) * 2
                 }rem)`,
             ),
           })}
