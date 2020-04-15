f38a43561cb5eab19f4ec6985fa8b1a18155c599
Anthony Yarbrough <asyarb@hawaii.edu>
2020-04-14 15:51:31 -1000

    feat: update to 12 nodes per page

--- gatsby-node.js
+++ gatsby-node.js
@@ -64,7 +64,7 @@ exports.createPages = async gatsbyContext => {
   const { createNode, createPage } = actions
   const { program } = store.getState()
 
-  const processPaginatedCollection = ({ collection, name, pageSize = 8 }) => {
+  const processPaginatedCollection = ({ collection, name, pageSize = 12 }) => {
     const nodeInput = createPaginatedCollectionNodes({
       collection,
       name,
