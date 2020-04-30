73ff9a93a49c6c58b439d05bef2d871c51c62af1
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-04-29 17:43:51 -1000

    fix: reduce width of ad-people message slice

--- src/pages/ad-people.tsx
+++ src/pages/ad-people.tsx
@@ -35,10 +35,12 @@ export const AdPeoplePage = ({ data, ...props }: AdPeoplePageProps) => {
         <Heading css={mq({ textAlign: 'center', fontSize: t.f.xl })}>
           Ad People of the Year
         </Heading>
+      </BoundedBox>
+
+      <BoundedBox maxWidth="Medium" css={{ backgroundColor: t.c.Gray95 }}>
         {/* temporary message start */}
-        <div css={mq({ marginTop: linearScale('1.5rem', '3.5rem') })}>
-          <MessageSlice
-            textHTML="<p>
+        <MessageSlice
+          textHTML="<p>
               Due to COVID19, judging for the 2020 Pele Awards has been
               postponed and will now be conducted virtually, and completed by the
               middle of this month. We plan to make an announcement of the finalists
@@ -47,8 +49,7 @@ export const AdPeoplePage = ({ data, ...props }: AdPeoplePageProps) => {
               completed mid-May. Any physical work that was entered with a
               submission will be returned to teachers.
               </p>"
-          />
-        </div>
+        />
         {/* temporary message end*/}
       </BoundedBox>
 
